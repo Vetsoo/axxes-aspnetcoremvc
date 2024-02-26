31011fa253330d354fe8443d681427c66ab779eb
Jente Vets <jente.vets@reynaers.com>
2024-02-26 12:07:02 +0100

    Solution step 2

---
+++ Excercise/Solution/Step 2/Axxes.Haxx/Axxes.Haxx.Web/Controllers/SessionController.cs
@@ -0,0 +1,50 @@
+﻿using Axxes.Haxx.EntityFramework;
+using Axxes.Haxx.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Axxes.Haxx.Web.Controllers
+{
+	[Authorize]
+	public class SessionController : BaseController
+    {
+        public SessionController(ApplicationDbContext db, UserManager<ApplicationUser> userManager) : base(db, userManager)
+        {
+        }
+
+        public IActionResult CreateSession()
+        {
+            return View();
+        }
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult CreateSession(SessionInputModel model)
+		{
+			if (model != null && ModelState.IsValid)
+			{
+				var session = new Session
+				{
+					SpeakerId = this.UserManager.GetUserId(this.User),
+					Title = model.Title,
+					DateTime = model.DateTime,
+					Category = model.Category,
+					Description = model.Description,
+					IsPublic = model.IsPublic
+				};
+
+				this.Db.Sessions.Add(session);
+				this.Db.SaveChanges();
+				return RedirectToAction("MySessions");
+			}
+
+			return View(model);
+		}
+
+		public IActionResult MySessions()
+		{
+			return View();
+		}
+	}
+}
---
+++ Excercise/Solution/Step 2/Axxes.Haxx/Axxes.Haxx.Web/Models/SessionDetailsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Axxes.Haxx.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Axxes.Haxx.Web.Models
+{
+	public class SessionDetailsViewModel
+	{
+		public int Id { get; set; }
+
+		public string Description { get; set; }
+
+		public string SpeakerId { get; set; }
+
+		public IEnumerable<CommentViewModel> Comments { get; set; }
+
+		public static Expression<Func<Session, SessionDetailsViewModel>> ViewModel
+		{
+			get
+			{
+				return e => new SessionDetailsViewModel()
+				{
+					Id = e.Id,
+					Description = e.Description,
+					Comments = e.Comments.AsQueryable().Select(CommentViewModel.ViewModel).ToList(),
+					SpeakerId = e.SpeakerId
+				};
+			}
+		}
+	}
+}
